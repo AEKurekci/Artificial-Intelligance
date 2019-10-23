df54d52a2fe9f3cb4c5ace64284bac6cded333da
Ali Emre Kürekci <aliemrekurekci@gmail.com>
2019-10-23 22:18:49 +0300

    Initial Commit

--- p2/kakuros.py
+++ p2/kakuros.py
@@ -36,18 +36,25 @@ thirdColumn = [x3, y3, z3]
 model.AddAllDifferent(firstColumn)
 model.AddAllDifferent(secondColumn)
 model.AddAllDifferent(thirdColumn)
-solver.Solve(model)
-solver.Value(z1 + z2 + z3 < 6)
 
-print(z1.Index())
-print(z2.Index())
-print(z3.Index())
+model.AddLinearConstraint(x1 + x2 + x3, 19, 20)
+model.AddLinearConstraint(y1 + y2 + y3, 18, 19)
+model.AddLinearConstraint(z1 + z2 + z3, 7, 8)
+model.AddLinearConstraint(x1 + y1 + x1, 21, 22)
+model.AddLinearConstraint(x2 + y2 + z2, 17, 18)
+model.AddLinearConstraint(x3 + y3 + z3, 6, 7)
+
+print(solver.Solve(model))
+
 print(x1.Index())
 print(x2.Index())
 print(x3.Index())
 print(y1.Index())
 print(y2.Index())
 print(y3.Index())
+print(z1.Index())
+print(z2.Index())
+print(z3.Index())
 
 
 
