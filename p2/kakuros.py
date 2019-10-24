8cd4a343fd8d1be36d9674077ba3e878112eef71
Ali Emre Kürekci <aliemrekurekci@gmail.com>
2019-10-24 18:35:32 +0300

    values are read from input text file and result is being writed output text file

--- p2/kakuros.py
+++ p2/kakuros.py
@@ -2,56 +2,65 @@ from ortools.sat.python import cp_model
 
 model = cp_model.CpModel()
 solver = cp_model.CpSolver()
+def Kakuro(borders, outputFile):
+    #x row values
+    x1 = model.NewIntVar(0, 9, "x1")
+    x2 = model.NewIntVar(0, 9, "x2")
+    x3 = model.NewIntVar(0, 9, "x3")
 
+    xList = [x1, x2, x3]
+    #x row values add as all diffirent
+    model.AddAllDifferent(xList)
 
-x1 = model.NewIntVar(0, 9, "x1")
-x2 = model.NewIntVar(0, 9, "x2")
-x3 = model.NewIntVar(0, 9, "x3")
+    #y row values
+    y1 = model.NewIntVar(0, 9, "y1")
+    y2 = model.NewIntVar(0, 9, "y2")
+    y3 = model.NewIntVar(0, 9, "y3")
 
-xList = [x1, x2, x3]
+    yList = [y1, y2, y3]
+    #y row values add as all diffirent
+    model.AddAllDifferent(yList)
+    #z row values
+    z1 = model.NewIntVar(0, 9, "z1")
+    z2 = model.NewIntVar(0, 9, "z2")
+    z3 = model.NewIntVar(0, 9, "z3")
 
-model.AddAllDifferent(xList)
+    zList = [z1, z2, z3]
+    #x row values add as all diffirent
+    model.AddAllDifferent(zList)
 
-y1 = model.NewIntVar(0, 9, "y1")
-y2 = model.NewIntVar(0, 9, "y2")
-y3 = model.NewIntVar(0, 9, "y3")
+    firstColumn = [x1, y1, z1]
+    secondColumn = [x2, y2, z2]
+    thirdColumn = [x3, y3, z3]
+    #all columns added to model as all different
+    model.AddAllDifferent(firstColumn)
+    model.AddAllDifferent(secondColumn)
+    model.AddAllDifferent(thirdColumn)
 
-yList = [y1, y2, y3]
+    model.Add(x1 + x2 + x3 == borders[3])
+    model.Add(y1 + y2 + y3 == borders[4])
+    model.Add(z1 + z2 + z3 == borders[5])
+    model.Add(x1 + y1 + z1 == borders[0])
+    model.Add(x2 + y2 + z2 == borders[1])
+    model.Add(x3 + y3 + z3 == borders[2])
 
-model.AddAllDifferent(yList)
+    #model solves
+    status = solver.Solve(model)
 
-z1 = model.NewIntVar(0, 9, "z1")
-z2 = model.NewIntVar(0, 9, "z2")
-z3 = model.NewIntVar(0, 9, "z3")
+    if status == cp_model.FEASIBLE:
+        outputFile.writelines(str(borders[3]) + ", " + str(solver.Value(x1)) + ", " + str(solver.Value(x2)) + ", " + str(solver.Value(x3)) + "\n")
+        outputFile.writelines(str(borders[4]) + ", " + str(solver.Value(y1)) + ", " + str(solver.Value(y2)) + ", " + str(solver.Value(y3)) + "\n")
+        outputFile.writelines(str(borders[5]) + ", " + str(solver.Value(z1)) + ", " + str(solver.Value(z2)) + ", " + str(solver.Value(z3)))
 
-zList = [z1, z2, z3]
+file = open("kakuro_input.txt", "r")
+line = file.readline()
+lineInt = []
 
-model.AddAllDifferent(zList)
-
-firstColumn = [x1, y1, z1]
-secondColumn = [x2, y2, z2]
-thirdColumn = [x3, y3, z3]
-
-model.AddAllDifferent(firstColumn)
-model.AddAllDifferent(secondColumn)
-model.AddAllDifferent(thirdColumn)
-
-model.Add(x1 + x2 + x3 == 20)
-model.Add(y1 + y2 + y3 == 19)
-model.Add(z1 + z2 + z3 == 8)
-model.Add(x1 + y1 + z1 == 22)
-model.Add(x2 + y2 + z2 == 18)
-model.Add(x3 + y3 + z3 == 7)
-
-status = solver.Solve(model)
-
-if status == cp_model.FEASIBLE:
-    print('x1 = %i' % solver.Value(x1))
-    print('x2 = %i' % solver.Value(x2))
-    print('x3 = %i' % solver.Value(x3))
-    print('y1 = %i' % solver.Value(y1))
-    print('y2 = %i' % solver.Value(y2))
-    print('y3 = %i' % solver.Value(y3))
-    print('z1 = %i' % solver.Value(z1))
-    print('z2 = %i' % solver.Value(z2))
-    print('z3 = %i' % solver.Value(z3))
+outputFile = open("kakuro_output.txt", "w")
+outputFile.writelines("x, " + line)
+while line:
+    line = line.strip().split(",")
+    for i in line:
+        lineInt.append(int(i))
+    line = file.readline()
+Kakuro(lineInt, outputFile)
