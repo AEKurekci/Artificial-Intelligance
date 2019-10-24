b1b9eaef2e3538467a6e4ecdae4cb95fe6dfccb2
Ali Emre Kürekci <aliemrekurekci@gmail.com>
2019-10-24 17:21:16 +0300

    Kakuros game is done with initial values

--- p2/kakuros.py
+++ p2/kakuros.py
@@ -1,7 +1,3 @@
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-
 from ortools.sat.python import cp_model
 
 model = cp_model.CpModel()
