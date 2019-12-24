325e9bdb98da7a4647fbf481646019b2f661230f
Ali Emre Kürekci <aliemrekurekci@gmail.com>
2019-12-24 14:15:06 +0300

    finding euklid results for all test element

--- p4/knn.py
+++ p4/knn.py
@@ -17,26 +17,40 @@ listOfDifferences = []
 listOfLastTrainItems = []
 listOfLastTestItems = []
 
+K = input("Please enter the K ")
+
 
 def difference(x1, y1):
-    print("difference ", abs(x1 - y1))
     return abs(x1 - y1)
 
 
-while trainLine:
-    train = trainLine.strip().split(',')
-    lastItemOfTrain = train[-1]
-    train.pop(-1)
-    for index, value in enumerate(test):
-        temp = difference(float(value), float(train[index]))
-        temp = temp ** 2
-        sumUp += temp
-    result = math.sqrt(sumUp)
-    listOfDifferences.append(result)
+while testLine:
+    while trainLine:
+        train = trainLine.strip().split(',')
+        lastItemOfTrain = train[-1]
+        train.pop(-1)
+        for index, value in enumerate(test):
+            temp = difference(float(value), float(train[index]))
+            temp = temp ** 2
+            sumUp += temp
+        result = math.sqrt(sumUp)
+        listOfDifferences.append(result)
+        listOfLastTrainItems.append(lastItemOfTrain)
+        sumUp = 0
+        trainLine = trainFile.readline()
     listOfLastTestItems.append(lastItemOfTest)
-    listOfLastTrainItems.append(lastItemOfTrain)
-    print("result ", result)
-    listOfDifferences.append(result)
-    sumUp = 0
+    testLine = testFile.readline()
+    test = testLine.strip().split(',')
+    lastItemOfTest = test[-1]
+    test.pop(-1)
+    trainFile = open("train.txt", "r")
+    trainLine = trainFile.readline()
     trainLine = trainFile.readline()
 
+print("differences ", listOfDifferences)
+print("trains ", listOfLastTrainItems)
+print("test ", listOfLastTestItems)
+
+print("differences ", len(listOfDifferences))
+print("trains ", len(listOfLastTrainItems))
+print("test ", len(listOfLastTestItems))
