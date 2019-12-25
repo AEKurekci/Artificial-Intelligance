53f7338b9b5c03cfc4972ac3b47de28052e1082c
Ali Emre Kürekci <aliemrekurekci@gmail.com>
2019-12-25 20:55:39 +0300

    differences to centers

--- p5/kmeans.py
+++ p5/kmeans.py
@@ -1,6 +1,8 @@
 from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
 import random
+import math
+
 file = open("data.txt", "r")
 line = file.readline()
 line = file.readline()
@@ -9,22 +11,44 @@ income = []
 
 K = int(input("Please enter K :"))
 
+
+def difference(val1, val2):
+    return abs(val1 - val2)
+
+
+def clustering(centersX, centersY, x, y):
+    listOfDifferences = []
+    for a in range(len(centersX)):
+        diffX = difference(x, centersX[a])
+        diffY = difference(y, centersY[a])
+        diffX = diffX ** 2
+        diffY = diffY ** 2
+        sumOFDiff = diffX + diffY
+        sumOFDiff = math.sqrt(sumOFDiff)
+        listOfDifferences.append(sumOFDiff)
+    print(listOfDifferences)
+    return listOfDifferences
+
+
 while line:
     line = line.strip().split(',')
-    income.append(line[0])
-    spend.append(line[1])
+    income.append(int(line[0]))
+    spend.append(int(line[1]))
     line = file.readline()
 plt.scatter(income, spend, s=2, marker='o')
 
 listOfCenters = []
 for counter in range(K):
-    rand = random.randrange(0, len(income) + 1)
+    rand = random.randrange(0, len(income))
     listOfCenters.append(rand)
-tempIncome = []
-tempSpend = []
+centerOfIncome = []
+centerOfSpend = []
 for i in listOfCenters:
-    tempIncome.append(income[i])
-    tempSpend.append(spend[i])
-plt.scatter(tempIncome, tempSpend, s=2, c='r', marker='o')
+    centerOfIncome.append(income[i])
+    centerOfSpend.append(spend[i])
+plt.scatter(centerOfIncome, centerOfSpend, s=2, c='r', marker='o')
+
+for j in range(len(income)):
+    clustering(centerOfIncome, centerOfSpend, income[j], spend[j])
 plt.show()
 exit(0)
