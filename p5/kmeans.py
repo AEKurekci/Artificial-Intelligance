9c0c023b4234b5310cbdb1db78980f9496c76a1d
Ali Emre Kürekci <aliemrekurekci@gmail.com>
2019-12-25 18:06:40 +0300

    reading data and plotting them

--- p5/kmeans.py
+++ p5/kmeans.py
@@ -1,2 +1,17 @@
-from matplotlib.backends.backend_pgf import PdfPages
+from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
+
+file = open("data.txt", "r")
+line = file.readline()
+line = file.readline()
+spend = []
+income = []
+while line:
+    line = line.strip().split(',')
+    income.append(line[0])
+    spend.append(line[1])
+    line = file.readline()
+
+plt.scatter(income, spend, s=2, c='b', marker='o')
+plt.show()
+exit(0)
