352db89cf45cdfa2a89162dcd6520fa2224e0d59
Ali Emre Kürekci <aliemrekurekci@gmail.com>
2019-12-25 21:17:41 +0300

    plotting different color for K

--- p5/kmeans.py
+++ p5/kmeans.py
@@ -2,6 +2,7 @@ from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
 import random
 import math
+import copy
 
 file = open("data.txt", "r")
 line = file.readline()
@@ -26,7 +27,6 @@ def clustering(centersX, centersY, x, y):
         sumOFDiff = diffX + diffY
         sumOFDiff = math.sqrt(sumOFDiff)
         listOfDifferences.append(sumOFDiff)
-    print(listOfDifferences)
     return listOfDifferences
 
 
@@ -35,20 +35,42 @@ while line:
     income.append(int(line[0]))
     spend.append(int(line[1]))
     line = file.readline()
-plt.scatter(income, spend, s=2, marker='o')
+plt.scatter(income, spend, s=2, c='k', marker='o')
 
 listOfCenters = []
 for counter in range(K):
     rand = random.randrange(0, len(income))
     listOfCenters.append(rand)
-centerOfIncome = []
-centerOfSpend = []
-for i in listOfCenters:
-    centerOfIncome.append(income[i])
-    centerOfSpend.append(spend[i])
-plt.scatter(centerOfIncome, centerOfSpend, s=2, c='r', marker='o')
+centerOfIncome = {}
+centerOfSpend = {}
+for i, k in enumerate(listOfCenters):
+    centerOfIncome[i] = income[i]
+    centerOfSpend[i] = spend[i]
+    if i == 0:
+        color = 'r'
+        listX = copy.deepcopy(list(centerOfIncome.values())[i])
+        listY = copy.deepcopy(list(centerOfSpend.values())[i])
+    elif i == 1:
+        color = 'g'
+        listX = copy.deepcopy(list(centerOfIncome.values())[i])
+        listY = copy.deepcopy(list(centerOfSpend.values())[i])
+    elif i == 2:
+        color = 'b'
+        listX = copy.deepcopy(list(centerOfIncome.values())[i])
+        listY = copy.deepcopy(list(centerOfSpend.values())[i])
+    elif i == 3:
+        color = 'm'
+        listX = copy.deepcopy(list(centerOfIncome.values())[i])
+        listY = copy.deepcopy(list(centerOfSpend.values())[i])
+    elif i == 4:
+        color = 'y'
+        listX = copy.deepcopy(list(centerOfIncome.values())[i])
+        listY = copy.deepcopy(list(centerOfSpend.values())[i])
+    plt.scatter(listX, listY, s=2, c=color, marker='o')
+print("center of income ", centerOfIncome)
 
 for j in range(len(income)):
-    clustering(centerOfIncome, centerOfSpend, income[j], spend[j])
+    listOFDiff = clustering(centerOfIncome, centerOfSpend, income[j], spend[j])
+
 plt.show()
 exit(0)
