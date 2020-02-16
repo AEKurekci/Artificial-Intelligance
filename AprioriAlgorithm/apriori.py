16c2643f0a2cfad8bd249068d159d883f16649bb
Ali Emre Kürekci <aliemrekurekci@gmail.com>
2020-02-16 23:11:13 +0300

    initial

--- AprioriAlgorithm/apriori.py
+++ AprioriAlgorithm/apriori.py
@@ -6,8 +6,28 @@ def init_Pass(T):
                 initList[j] = 1
             else:
                 initList[j] += 1
-    return initList
+    sortedInitList = {}
+    for key in initList.keys():
+        sortedInitList[key] = initList[key]
+    initList.clear()
+    return sortedInitList
+
+
+def candidate_gen(F):
+    C = {}
+    for i in F:
+        if len(i) == 1:
+            print("candidate")
+    pass
+
 
 def apriori(Transaction, minSup):
     C1 = init_Pass(Transaction)
-    F1 =
+    allF = []
+    Frequent = {}
+    for i, j in C1.items():
+        if (j / len(Transaction)) > minSup:
+            Frequent[i] = j
+    allF.append(Frequent)
+    while len(Frequent) != 0:
+        Candidates = candidate_gen(Frequent)
